207d1559493daa88c7e5750a081de11eb66d64eb
Alexander Kraemling <alexander.kraemling@gmail.com>
2024-06-24 22:10:45 +0200

    Initial Commit

---
+++ src/p3.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class p3 {
+    public static void main(String[] args) {
+        String USERID = "";
+        String PASSWORD = "";
+        String CONNECTIONSTRING = "";
+        Connection connection = null;
+    }
+
+    public static void locations (Connection connection) {
+
+    }
+
+    public static void edges (Connection connection) {
+
+    }
+
+    public static void csstaff (Connection connection) {
+
+    }
+
+    public static void insertPhone (Connection connection) {
+
+    }
+}
