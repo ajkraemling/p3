0382905d63c470779eebfc4f9b6bd934baaab353
Alexander Kraemling <alexander.kraemling@gmail.com>
2024-06-25 14:54:42 +0200

    Finished 1 and 2

--- src/p3.java
+++ src/p3.java
@@ -5,14 +5,17 @@ public class p3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter USERID: ");
-        String USERID = scanner.nextLine();
+        System.out.println("Enter with syntax: java p3 <username> <password> <connection string> <menu item>");
+        String userInput = scanner.nextLine();
+        String[] tokens = userInput.split(" ");
 
-        System.out.print("Enter PASSWORD: ");
-        String PASSWORD = scanner.nextLine();
+        String USERID = tokens[2];
 
-        System.out.print("Enter CONNECTIONSTRING: ");
-        String CONNECTIONSTRING = scanner.nextLine(); // jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl
+        String PASSWORD = tokens[3];
+
+        String CONNECTIONSTRING = tokens[4]; // jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl
+//        java p3 ajkraemling MelonFighter1*1 jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl 2
+        String menuItem = tokens[5];
 
         System.out.println("-------Oracle JDBC Connection Testing ---------");
         try {
@@ -39,8 +42,6 @@ public class p3 {
         }
         System.out.println("Oracle JDBC Driver Connected!");
 
-        System.out.println("Choose a menu item: \n1 - Report Location Information\n2 - Report Edge Information\n3 - Report CS Staff Information\n4 - Insert New Phone Extension");
-        String menuItem = scanner.nextLine();
         switch (menuItem) {
             case "1":
                 locations(connection);
@@ -66,9 +67,10 @@ public class p3 {
             System.out.print("Enter Location ID: ");
             String locationID = scanner.nextLine();
 
-            Statement stmt = connection.createStatement();
-            String str = "SELECT * FROM LOCATIONS";
-            ResultSet rset = stmt.executeQuery(str);
+            String query = "SELECT * FROM LOCATIONS WHERE LOCATIONID = ?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(1, locationID);
+            ResultSet rset = pstmt.executeQuery();
 
             String locID = "";
             String locationName = "";
@@ -88,17 +90,53 @@ public class p3 {
             } // end while
 
             rset.close();
-            stmt.close();
+            pstmt.close();
             connection.close();
         } catch (SQLException e) {
-            System.out.println("Get Data Failed! Check output console");
+            System.out.println("Error: Could not find location ID");
             e.printStackTrace();
             return;
         }
     }
 
     public static void edges(Connection connection) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Edge ID: ");
+            String edgeIN = scanner.nextLine();
 
+            String query = "SELECT EDGES.EDGEID, L1.LOCATIONNAME AS StartLocationName, L1.MAPFLOOR AS StartLocationFloor, L2.LOCATIONNAME AS EndLocationName, L2.MAPFLOOR AS EndLocationFloor " +
+                                "FROM EDGES " +
+                                "JOIN LOCATIONS L1 ON L1.LOCATIONID = EDGES.STARTINGLOCATIONID " +
+                                "JOIN LOCATIONS L2 ON L2.LOCATIONID = EDGES.ENDINGLOCATIONID " +
+                                "WHERE EDGES.EDGEID = ?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(1, edgeIN);
+            ResultSet rset = pstmt.executeQuery();
+
+            String edgeID = "";
+            String stLocationName = "";
+            String stMapFloor = "";
+            String endLocationName = "";
+            String endMapFloor = "";
+            // Process the results
+            while (rset.next()) {
+                edgeID = rset.getString("EDGEID");
+                stLocationName = rset.getString("StartLocationName");
+                stMapFloor = rset.getString("StartLocationFloor");
+                endLocationName = rset.getString("EndLocationName");
+                endMapFloor = rset.getString("EndLocationFloor");
+                System.out.println("Edges Information\nEdge ID: " + edgeID + "\nStarting Location Name: " + stLocationName + "\nStarting Location Floor: " + stMapFloor + "\nEnding Location Name: " + endLocationName + "\nEnding Location Floor: " + endMapFloor);
+            } // end while
+
+            rset.close();
+            pstmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Error: Could not find edge ID");
+            e.printStackTrace();
+            return;
+        }
     }
 
     public static void csstaff(Connection connection) {
